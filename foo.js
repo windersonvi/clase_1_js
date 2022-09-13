1ec32e246c9f0ad298f38447a14dada4ad7a2559
windersonvi <windersonvo@gmail.com>
2022-09-12 22:34:35 -0400

    fc

---
+++ foo.js
@@ -0,0 +1,11 @@
+//console.log("Hola mundo")
+let nombre = "Winderson Villarreal"
+var edad = 10
+let mayorDeEdad = 21
+const NRO_PASAPORTE = 28756556
+
+
+console.log("Nombre:",nombre)
+console.log("Edad:",edad)
+console.log("Mayor de edad:",mayorDeEdad)
+
